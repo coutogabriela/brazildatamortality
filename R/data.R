2363c24d889433036474c18f349c490d3cc159eb
albhasan <albhasan@gmail.com>
2021-06-10 10:23:08 -0300

    Added translation of variable names.

--- R/data.R
+++ R/data.R
@@ -1,116 +1,129 @@
-#' Deaths due to natural disaster in Brazil from 1996 to 2019.
 #'
-#' A dataset containing information regarding deaths in Brazil due to natural
-#' disasters from 1996 to 2019.
+#'
+#' Original source:
+#' World Health Organization
+#' International Statistic Classification of Diseases and Related Health Problems 10th revision
+#' ICD10
+#'
+#' Based on death certificate that captures more than 50 variables as:
+#' entity and municipality of registry,
+#' entity,
+#' municipality and locality of habitual residence;
+#' entity, municipality and locality of occurrence of death; sex, age, occupation, schooling, conjugal status, among others
+#'
+#' Deaths due to natural events in Brazil from 1996 to 2019.
+#'
+#' A dataset containing information of causes of death those caused by exposure to forces of nature
+#' CID10 from 1996 to 2019
 #'
 #'
 #' @format A data frame with 5862 rows and 105 variables:
 #' \describe{
 #' \item{file_path}{Path to the file from which the data is comming from.}
-#' \item{NUMERODO}{TODO: Add description}
-#' \item{TIPOBITO}{TODO: Add description}
-#' \item{DTOBITO}{TODO: Add description}
-#' \item{NATURAL}{TODO: Add description}
-#' \item{DTNASC}{TODO: Add description}
-#' \item{IDADE}{TODO: Add description}
-#' \item{SEXO}{TODO: Add description}
-#' \item{RACACOR}{TODO: Add description}
-#' \item{ESTCIV}{TODO: Add description}
-#' \item{ESC}{TODO: Add description}
-#' \item{OCUP}{TODO: Add description}
-#' \item{CODBAIRES}{TODO: Add description}
-#' \item{CODMUNRES}{TODO: Add description}
-#' \item{LOCOCOR}{TODO: Add description}
-#' \item{CODMUNOCOR}{TODO: Add description}
-#' \item{IDADEMAE}{TODO: Add description}
-#' \item{ESCMAE}{TODO: Add description}
-#' \item{OCUPMAE}{TODO: Add description}
-#' \item{QTDFILVIVO}{TODO: Add description}
-#' \item{QTDFILMORT}{TODO: Add description}
-#' \item{GRAVIDEZ}{TODO: Add description}
-#' \item{GESTACAO}{TODO: Add description}
-#' \item{PARTO}{TODO: Add description}
-#' \item{OBITOPARTO}{TODO: Add description}
-#' \item{PESO}{TODO: Add description}
-#' \item{OBITOGRAV}{TODO: Add description}
-#' \item{OBITOPUERP}{TODO: Add description}
-#' \item{ASSISTMED}{TODO: Add description}
-#' \item{EXAME}{TODO: Add description}
-#' \item{CIRURGIA}{TODO: Add description}
-#' \item{NECROPSIA}{TODO: Add description}
-#' \item{CAUSABAS}{TODO: Add description}
-#' \item{LINHAA}{TODO: Add description}
-#' \item{LINHAB}{TODO: Add description}
-#' \item{LINHAC}{TODO: Add description}
-#' \item{LINHAD}{TODO: Add description}
-#' \item{LINHAII}{TODO: Add description}
-#' \item{CIRCOBITO}{TODO: Add description}
-#' \item{ACIDTRAB}{TODO: Add description}
-#' \item{FONTE}{TODO: Add description}
-#' \item{date}{TODO: Add description}
-#' \item{CODESTAB}{TODO: Add description}
-#' \item{ATESTANTE}{TODO: Add description}
-#' \item{UFINFORM}{TODO: Add description}
-#' \item{HORAOBITO}{TODO: Add description}
-#' \item{CODBAIOCOR}{TODO: Add description}
-#' \item{NUMERODN}{TODO: Add description}
-#' \item{TPASSINA}{TODO: Add description}
-#' \item{DTATESTADO}{TODO: Add description}
-#' \item{TPPOS}{TODO: Add description}
-#' \item{DTINVESTIG}{TODO: Add description}
-#' \item{CAUSABAS_O}{TODO: Add description}
-#' \item{DTCADASTRO}{TODO: Add description}
-#' \item{FONTEINV}{TODO: Add description}
-#' \item{DTRECEBIM}{TODO: Add description}
-#' \item{CODINST}{TODO: Add description}
-#' \item{CB_PRE}{TODO: Add description}
-#' \item{MORTEPARTO}{TODO: Add description}
-#' \item{TPOBITOCOR}{TODO: Add description}
-#' \item{ORIGEM}{TODO: Add description}
-#' \item{DTCADINF}{TODO: Add description}
-#' \item{DTCADINV}{TODO: Add description}
-#' \item{NUMERODV}{TODO: Add description}
-#' \item{NUMSUS}{TODO: Add description}
-#' \item{COMUNSVOIM}{TODO: Add description}
-#' \item{DTRECORIG}{TODO: Add description}
-#' \item{DTRECORIGA}{TODO: Add description}
-#' \item{CAUSAMAT}{TODO: Add description}
-#' \item{ESC2010}{TODO: Add description}
-#' \item{ESCMAE2010}{TODO: Add description}
-#' \item{STDOEPIDEM}{TODO: Add description}
-#' \item{STDONOVA}{TODO: Add description}
-#' \item{CODMUNCART}{TODO: Add description}
-#' \item{CODCART}{TODO: Add description}
-#' \item{NUMREGCART}{TODO: Add description}
-#' \item{DTREGCART}{TODO: Add description}
-#' \item{SERIESCFAL}{TODO: Add description}
-#' \item{ESCMAEAGR1}{TODO: Add description}
-#' \item{ESCFALAGR1}{TODO: Add description}
-#' \item{SERIESCMAE}{TODO: Add description}
-#' \item{SEMAGESTAC}{TODO: Add description}
-#' \item{TPMORTEOCO}{TODO: Add description}
-#' \item{EXPDIFDATA}{TODO: Add description}
-#' \item{DIFDATA}{TODO: Add description}
-#' \item{DTCONINV}{TODO: Add description}
-#' \item{DTCONCASO}{TODO: Add description}
-#' \item{NUDIASOBIN}{TODO: Add description}
-#' \item{CODMUNNATU}{TODO: Add description}
-#' \item{ESTABDESCR}{TODO: Add description}
-#' \item{CRM}{TODO: Add description}
-#' \item{NUMEROLOTE}{TODO: Add description}
-#' \item{STCODIFICA}{TODO: Add description}
-#' \item{CODIFICADO}{TODO: Add description}
-#' \item{VERSAOSIST}{TODO: Add description}
-#' \item{VERSAOSCB}{TODO: Add description}
-#' \item{ATESTADO}{TODO: Add description}
-#' \item{NUDIASOBCO}{TODO: Add description}
-#' \item{FONTES}{TODO: Add description}
-#' \item{TPRESGINFO}{TODO: Add description}
-#' \item{TPNIVELINV}{TODO: Add description}
-#' \item{NUDIASINF}{TODO: Add description}
-#' \item{FONTESINF}{TODO: Add description}
-#' \item{ALTCAUSA}{TODO: Add description}
-#' \item{CONTADOR}{TODO: Add description}
+#' \item{NUMERODO}{DEATH CERTIFICATE NUMBER}
+#' \item{TIPOBITO}{FETAL}
+#' \item{DTOBITO}{DATE}
+#' \item{NATURAL}{CITIZENSHIP}
+#' \item{DTNASC}{BIRTH}
+#' \item{IDADE}{AGE}
+#' \item{SEXO}{SEX}
+#' \item{RACACOR}{RACE}
+#' \item{ESTCIV}{MARITAL}
+#' \item{ESC}{EDUCATION}
+#' \item{OCUP}{JOB}
+#' \item{CODBAIRES}{NEIGHBOURHOOD}
+#' \item{CODMUNRES}{CITY}
+#' \item{LOCOCOR}{DEATH PLACE}
+#' \item{CODMUNOCOR}{CITY CODE}
+#' \item{IDADEMAE}{MOTHER AGE)
+#' \item{ESCMAE}{MOTHER EDUCATION}
+#' \item{OCUPMAE}{MOTHER JOB}
+#' \item{QTDFILVIVO}{N.CHILDREN}
+#' \item{QTDFILMORT}{DELETE}
+#' \item{GRAVIDEZ}{DELETE}
+#' \item{GESTACAO}{DELETE}
+#' \item{PARTO}{{DELETE}
+#' \item{OBITOPARTO}{DELETE}
+#' \item{PESO}{DELETE}
+#' \item{OBITOGRAV}{DELETE}
+#' \item{OBITOPUERP}{DELETE}
+#' \item{ASSISTMED}{HEALTH CARE}
+#' \item{EXAME}{DELETE}
+#' \item{CIRURGIA}{DELETE}
+#' \item{NECROPSIA}{DELETE}
+#' \item{CAUSABAS}{CAUSE}
+#' \item{LINHAA}{DELETE}
+#' \item{LINHAB}{DELETE}
+#' \item{LINHAC}{DELETE}
+#' \item{LINHAD}{DELETE}
+#' \item{LINHAII}{DELETE}
+#' \item{CIRCOBITO}{DELETE}
+#' \item{ACIDTRAB}{DELETE}
+#' \item{FONTE}{SOURCE}
+#' \item{date}{DELETE}
+#' \item{CODESTAB}{DELETE}
+#' \item{ATESTANTE}{DELETE}
+#' \item{UFINFORM}{DELETE}
+#' \item{HORAOBITO}{DELETE}
+#' \item{CODBAIOCOR}{DELETE}
+#' \item{NUMERODN}{DELETE}
+#' \item{TPASSINA}{DELETE}
+#' \item{DTATESTADO}{DELETE}
+#' \item{TPPOS}{TODO:{DELETE}
+#' \item{DTINVESTIG}{DELETE}
+#' \item{CAUSABAS_O}{CAUSE}
+#' \item{DTCADASTRO}{DELETE}
+#' \item{FONTEINV}{DELETE}
+#' \item{DTRECEBIM}{DELETE}
+#' \item{CODINST}{DELETE}
+#' \item{CB_PRE}{DELETE}
+#' \item{MORTEPARTO}{DELETE}
+#' \item{TPOBITOCOR}{DELETE}
+#' \item{ORIGEM}{DELETE}
+#' \item{DTCADINF}{DELETE}
+#' \item{DTCADINV}{DELETE}
+#' \item{NUMERODV}{DELETE}
+#' \item{NUMSUS}{DELETE}
+#' \item{COMUNSVOIM}{DELETE}
+#' \item{DTRECORIG}{DELETE}
+#' \item{DTRECORIGA}{DELETE}
+#' \item{CAUSAMAT}{DELETE}
+#' \item{ESC2010}{DELETE}
+#' \item{ESCMAE2010}{DELETE}
+#' \item{STDOEPIDEM}{DELETE}
+#' \item{STDONOVA}{DELETE}
+#' \item{CODMUNCART}{DELETE}
+#' \item{CODCART}{DELETE}
+#' \item{NUMREGCART}{DELETE}
+#' \item{DTREGCART}{DELETE}
+#' \item{SERIESCFAL}{DELETE}
+#' \item{ESCMAEAGR1}{DELETE}
+#' \item{ESCFALAGR1}{DELETE}
+#' \item{SERIESCMAE}{DELETE}
+#' \item{SEMAGESTAC}{DELETE}
+#' \item{TPMORTEOCO}{DELETE}
+#' \item{EXPDIFDATA}{DELETE}
+#' \item{DIFDATA}{DELETE}
+#' \item{DTCONINV}{DELETE}
+#' \item{DTCONCASO}{DELETE}
+#' \item{NUDIASOBIN}{DELETE}
+#' \item{CODMUNNATU}{DELETE}
+#' \item{ESTABDESCR}{DELETE}
+#' \item{CRM}{DELETE}
+#' \item{NUMEROLOTE}{DELETE}
+#' \item{STCODIFICA}{DELETE}
+#' \item{CODIFICADO}{DELETE}
+#' \item{VERSAOSIST}{DELETE}
+#' \item{VERSAOSCB}{DELETE}
+#' \item{ATESTADO}{DELETE}
+#' \item{NUDIASOBCO}{DELETE}
+#' \item{FONTES}{DELETE}
+#' \item{TPRESGINFO}{DELETE}
+#' \item{TPNIVELINV}{DELETE}
+#' \item{NUDIASINF}{DELETE}
+#' \item{FONTESINF}{DELETE}
+#' \item{ALTCAUSA}{DELETE}
+#' \item{CONTADOR}{DELETE}
 #' }
 #' @source \url{http://www.TODO_update_url.info/}
 "data_icd_10"
